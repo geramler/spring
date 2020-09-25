fe40e91d1a69740d8b7b4fa74e014de884593ec6
Gerardo Ramírez <geramler@gmail.com>
2020-09-25 14:07:29 -0500

    Updated Project

--- cems/src/test/java/com/cems/ApplicationTests.java
+++ cems/src/test/java/com/cems/ApplicationTests.java
@@ -1,10 +1,10 @@
 package com.cems;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
